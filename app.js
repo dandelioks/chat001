3debaae40827c06a3d7d90115f9c689f0c0dbe42
mockingraven <mockingrave@gmail.com>
2020-06-17 10:22:15 +0300

    test2

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ var http = require('http');
 var fs = require('fs');
 var config = require('./config/index');
 var log = require('./lib/log')(module);
-//var mongoose = require('lib/mongoose');
-var HttpError = require('error').HttpError;
+var mongoose = require('./lib/mongoose');
+var HttpError = require('./error/index').HttpError;
 var bodyParser  =  require ( 'body-parser' )
 var  multer   =  require ( 'multer' )
 var  favicon  =  require ( 'serve-favicon' ) //иконка нам пока не нужна
 var debug = require('debug')('app4');
 var  session  =  require ( 'express-session' )
-//var sessionStore = require('./lib/sessionStore');
+var sessionStore = require('./lib/sessionStore');
 
 var app = express();
 
 app.set('views', __dirname + '/template');
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 //app.use(favicon()); иконка нам пока не нужна
 
@@ -40,16 +40,16 @@ app.use(bodyParser.text( // выползло из body parser'a
 
 app.use(cookieParser());
 
-/*
+
 app.use(session({
   secret: config.get('session:secret'),
   key: config.get('session:key'),
   cookie: config.get('session:cookie'),
   store: sessionStore
-}));*/
+}));
 
 app.use(require('./middleware/sendHttpError'));
-//app.use(require('./middleware/loadUser'));
+app.use(require('./middleware/loadUser'));
 
 //app.use(app.router);
 
